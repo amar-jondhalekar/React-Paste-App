474ab5576cb8ce4582abc9fb924c9ee6652172a2
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 14:53:31 +0530

    added useState for value, searchParam to useSearchParams and also added conditional rendering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState("");
+    const [value, setValue] = useState(''); 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const pasteId = searchParams.get("pasteId"); 
   return (
     <div>
-        <input className='' 
+        <input className='p-2 rounded-2xl mt-2' 
         type="text"
         placeholder='Enter title here'
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         />
+        <button>
+            {
+                pasteId ? "Update My Paste" : "Create My Paste"
+            }
+        </button>
     </div>
   )
 }
