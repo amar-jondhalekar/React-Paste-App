d1b8c16148c644734b88975c184701201d20a0e4
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 15:06:17 +0530

    added tailwind classes to styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     const pasteId = searchParams.get("pasteId"); 
   return (
    <div>
-     <div className='flex flex-row gap-7'>
+     <div className='flex flex-row gap-7 place-content-between'>
         <input className='p-2 rounded-2xl mt-2' 
         type="text"
         placeholder='Enter title here'
@@ -21,8 +21,8 @@ const Home = () => {
             }
         </button>
     </div>
-    <div>
-        <textarea
+    <div className='mt-8'>
+        <textarea className='rounded-2xl mt-4 min-w-[500px] p-4'
         value={value}
         placeholder='Enter content here'
         onChange={(e) => setValue(e.target.value)}
