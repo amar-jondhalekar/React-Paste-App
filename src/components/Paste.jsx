af24a5eb3fd77f3570d326939ed8a5097b46711a
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 16:22:18 +0530

    display all pastes data to console

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Paste = () => {
+
+    const pastes = useSelector((state) => state.paste.pastes);
+    console.log(pastes);
+
   return (
     <div>List of pastes </div>
   )
