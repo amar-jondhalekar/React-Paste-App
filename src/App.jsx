8f04d5ed16fcc95849185e136cf3006b9c39152b
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 13:59:51 +0530

    added router provider and reducer function and passed component to createBrowser router element

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 import { useState } from 'react'
 import './App.css'
+import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Paste from './components/Paste'
+import ViewPaste from './components/ViewPaste';
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:
+      <div>
+        <Navbar/>
+        <Home/>
+      </div>
+    },
+    {
+      path:"/pastes",
+      element: 
+      <div>
+        <Navbar/>
+        <Paste/>
+      </div>
+    },
+    {
+      path:"pastes/:id",
+      element:
+      <div>
+        <Navbar/>
+        <ViewPaste/>
+      </div>
+    }
+  ]
+);
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className="text-3xl font-bold underline">
-      Hello world!
-    </div>
+    <RouterProvider router={router}/>
     </>
   )
 }
