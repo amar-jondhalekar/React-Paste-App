c30836912c69a77fbff8558900a5872107abd0b4
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 11:10:50 +0530

    imported pasteRudecer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import pasteReducer from '../src/redux/pasteSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    paste: pasteReducer,
+  },
 })
