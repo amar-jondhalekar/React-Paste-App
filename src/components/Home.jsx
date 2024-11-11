bbefb32e17cde766a6897af2fbb81208a3ea84cb
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 15:17:05 +0530

    created new function i.e. createPaste and also added useDispatch hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
@@ -6,16 +7,34 @@ const Home = () => {
     const [value, setValue] = useState(''); 
     const [searchParams, setSearchParams] = useSearchParams();
     const pasteId = searchParams.get("pasteId"); 
+    const dispatch = useDispatch();
+    function createPaste(){
+        const paste = {
+            title: title,
+            content: value,
+            _id: pasteId ||
+                Date.now().toISOString(),
+        }
+        if(pasteId){
+            // update
+            dispatch();
+        }
+        else{
+            // create 
+            dispatch();
+        }
+    }
+
   return (
    <div>
      <div className='flex flex-row gap-7 place-content-between'>
-        <input className='p-2 rounded-2xl mt-2' 
+        <input className='p-1 rounded-2xl mt-2 w-[60%] pl-4' 
         type="text"
         placeholder='Enter title here'
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         />
-        <button className='p-2 rounded-2xl mt-2'>
+        <button onClick={createPaste} className='p-2 rounded-2xl mt-2'>
             {
                 pasteId ? "Update My Paste" : "Create My Paste"
             }
