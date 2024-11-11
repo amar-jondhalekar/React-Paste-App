0d80716557bb492a7b7d61fa18b365345b812848
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 16:32:12 +0530

    import React from 'react' import { useSelector } from 'react-redux'

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Paste = () => {
 
     const pastes = useSelector((state) => state.paste.pastes);
     console.log(pastes);
+    const [searchTerm, setSearchTerm] = useState('');
+    const dispatch = useDispatch();
+
+    const filteredData = pastes.filter((paste) => paste.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
-    <div>List of pastes </div>
+    <div>
+        <input type="search"
+        className='p-2 rounded-2xl min-w-[600px] mt-5' 
+        placeholder='search here'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        />
+    </div>
   )
 }
 
