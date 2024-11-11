ef6622945be2b514e72c2067ce49f824939de826
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-11 15:17:25 +0530

    added tailwind classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row gap-4'>
+    <div className='flex flex-row gap-4 place-content-between'>
         <NavLink
         to="/"
         >
