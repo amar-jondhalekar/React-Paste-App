5a0e57e6bfb7f7fee11e25ebb3126a8e6d700d02
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-12 09:17:20 +0530

    #fixed: content render for upating value

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,63 +1,71 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-import { addToPastes, updateToPastes } from '../redux/pasteSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { addToPastes, updateToPastes } from "../redux/pasteSlice";
 
 const Home = () => {
-    const [title, setTitle] = useState("");
-    const [value, setValue] = useState(''); 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const pasteId = searchParams.get("pasteId"); 
-    const dispatch = useDispatch();
-    function createPaste(){
-        const paste = {
-            title: title,
-            content: value,
-            _id: pasteId ||
-            new Date().toISOString(),
-            createdAt: pasteId ? undefined : new Date().toISOString()  // Set createdAt only if it's a new paste
-        };
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pasteId = searchParams.get("pasteId");
+  const dispatch = useDispatch();
+  const allPastes = useSelector((state) => state.paste.pastes);
 
-        if(pasteId){
-            // update
-            dispatch(updateToPastes(paste));
-        }
-        else{
-            // create 
-            dispatch(addToPastes(paste));
-        }
+  useEffect(() => {
+    if (pasteId) {
+      const paste = allPastes.find((p) => p._id === pasteId);
+      setTitle(paste.title);
+      setValue(paste.content);
+    }
+  }, [pasteId]);
 
-        // after creation or updation 
-        setTitle('');
-        setValue('');
-        setSearchParams({})
+  function createPaste() {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId || new Date().toISOString(),
+      createdAt: pasteId ? undefined : new Date().toISOString(), // Set createdAt only if it's a new paste
+    };
+
+    if (pasteId) {
+      // update
+      dispatch(updateToPastes(paste));
+    } else {
+      // create
+      dispatch(addToPastes(paste));
     }
 
-  return (
-   <div>
-     <div className='flex flex-row gap-7 place-content-between'>
-        <input className='p-1 rounded-2xl mt-2 w-[60%] pl-4' 
-        type="text"
-        placeholder='Enter title here'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        />
-        <button onClick={createPaste} className='p-2 rounded-2xl mt-2'>
-            {
-                pasteId ? "Update My Paste" : "Create My Paste"
-            }
-        </button>
-    </div>
-    <div className='mt-8'>
-        <textarea className='rounded-2xl mt-4 min-w-[500px] p-4'
-        value={value}
-        placeholder='Enter content here'
-        onChange={(e) => setValue(e.target.value)}
-        rows={20}
-        />
-    </div>
-   </div>
-  )
-}
+    // after creation or updation
+    setTitle("");
+    setValue("");
+    setSearchParams({});
+  }
 
-export default Home
+  return (
+    <div>
+      <div className="flex flex-row gap-7 place-content-between">
+        <input
+          className="p-1 rounded-2xl mt-2 w-[60%] pl-4"
+          type="text"
+          placeholder="Enter title here"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button onClick={createPaste} className="p-2 rounded-2xl mt-2">
+          {pasteId ? "Update My Paste" : "Create My Paste"}
+        </button>
+      </div>
+      <div className="mt-8">
+        <textarea
+          className="rounded-2xl mt-4 min-w-[500px] p-4"
+          value={value}
+          placeholder="Enter content here"
+          onChange={(e) => setValue(e.target.value)}
+          rows={20}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
