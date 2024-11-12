b812d56e1710fbdd46b6f8d68b295512dcf1d5fa
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-12 08:51:33 +0530

    added: handleDelete function to delete paste

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { removeFromPastes } from '../redux/pasteSlice';
 
 const Paste = () => {
   const pastes = useSelector((state) => state.paste.pastes);
@@ -12,6 +13,10 @@ const Paste = () => {
   const filteredData = pastes.filter((paste) =>
     paste.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  function handleDelete(pasteId){
+    dispatch(removeFromPastes(pasteId));
+  }
 
   return (
     <div>
@@ -36,7 +41,7 @@ const Paste = () => {
                 <div className="flex flex-row gap-4 place-content-evenly mt-3">
                   <button>Edit</button>
                   <button>View</button>
-                  <button>Delete</button>
+                  <button onClick={() => handleDelete(paste?._id)}>Delete</button>
                   <button>Copy</button>
                   <button>Share</button>
                 </div>
